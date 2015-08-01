15a32039578432b708e5cb8886e8c9fcc1ff5893
garajm <garaj.michael@gmail.com>
2015-08-02 00:01:26 +0200

    Lesson 7

--- src/main/java/sk/garaj/spring/tutorial/app/Person.java
+++ src/main/java/sk/garaj/spring/tutorial/app/Person.java
@@ -1,7 +1,24 @@
 package sk.garaj.spring.tutorial.app;
 
 public class Person {
+	
+	private int id;
+	private String name;
+	
+	public Person() {
+		
+	}
+	
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
 	public void speak() {
 		System.out.println("Hello! I am a person.");
+	}
+	
+	public String toString() {
+		return "Person [id=" + this.id + ", name=" + this.name + "]";
 	}
 }
