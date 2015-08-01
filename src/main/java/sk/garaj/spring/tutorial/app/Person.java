7d6aac7b5b8ac79cf2dcd4205c0e66d2b61c7d24
garajm <garaj.michael@gmail.com>
2015-08-02 00:08:36 +0200

    Lesson 8

--- src/main/java/sk/garaj/spring/tutorial/app/Person.java
+++ src/main/java/sk/garaj/spring/tutorial/app/Person.java
@@ -5,6 +5,8 @@ public class Person {
 	private int id;
 	private String name;
 	
+	private int taxId;
+	
 	public Person() {
 		
 	}
@@ -19,6 +21,14 @@ public class Person {
 	}
 	
 	public String toString() {
-		return "Person [id=" + this.id + ", name=" + this.name + "]";
+		return "Person [id=" + this.id + ", name=" + this.name + ", taxId=" + this.taxId + "]";
+	}
+
+	public int getTaxId() {
+		return taxId;
+	}
+
+	public void setTaxId(int taxId) {
+		this.taxId = taxId;
 	}
 }
