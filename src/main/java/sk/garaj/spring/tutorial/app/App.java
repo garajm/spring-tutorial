bc389f4fa2dbc8221974b1e527ab4c354c8cb39b
garajm <garaj.michael@gmail.com>
2015-08-02 22:55:31 +0200

    Lesson 49

--- src/main/java/sk/garaj/spring/tutorial/app/App.java
+++ src/main/java/sk/garaj/spring/tutorial/app/App.java
@@ -1,5 +1,6 @@
 package sk.garaj.spring.tutorial.app;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
@@ -12,6 +13,15 @@ public class App {
 		ApplicationContext context = new ClassPathXmlApplicationContext("sk/garaj/spring/tutorial/app/beans/beans.xml");
 		
 		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
+		
+		Offer offer1 = new Offer(9, "Claire", "claire@caveofprograming.com", "Coding in Python");
+		Offer offer2 = new Offer(8, "Thomas", "thomas@caveofprograming.com", "Coding in Ruby on Rails");
+		
+		List<Offer> batchOffers = new ArrayList<Offer>();
+		batchOffers.add(offer1);
+		batchOffers.add(offer2);
+		
+		offersDao.create(batchOffers);
 		
 		List<Offer> offers = offersDao.getOffers();
 		
--- src/main/java/sk/garaj/spring/tutorial/app/OffersDAO.java
+++ src/main/java/sk/garaj/spring/tutorial/app/OffersDAO.java
@@ -7,18 +7,48 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component("offersDao")
 public class OffersDAO {
 
-	private JdbcTemplate jdbc;
+	private NamedParameterJdbcTemplate jdbc;
 
 	@Autowired
 	public void setDataSource(DataSource jdbc) {
-		this.jdbc = new JdbcTemplate(jdbc);
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+	}
+	
+	public boolean create(Offer offer) {
+		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
+
+		return jdbc.update("insert into offers (name, text, email) values (:name, :text, :email)", params) == 1;
+	}
+
+	@Transactional
+	public int[] create(List<Offer> offers) {
+		SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offers.toArray());
+		
+		return jdbc.batchUpdate("insert into offers (id, name, text, email) values (:id, :name, :text, :email)", params);
+	}
+	
+	public boolean update(Offer offer) {
+		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
+
+		return jdbc.update("update offers set name=:name, text=:text, email=:email where id=:id", params) == 1;
+	}
+	
+	public boolean delete(int id) {
+		MapSqlParameterSource params = new MapSqlParameterSource("id", id);
+		
+		return jdbc.update("delete from offers where id = :id", params) == 1;
 	}
 
 	public List<Offer> getOffers() {
@@ -35,6 +65,26 @@ public class OffersDAO {
 			}
 
 		});
+	}
+	
+	public Offer getOffer(int id) {
+		MapSqlParameterSource params =  new MapSqlParameterSource("id", id);
+		
+		List<Offer> offers = jdbc.query("select * from offers where id = :id", params, new RowMapper<Offer>() {
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				
+				offer.setId(rs.getInt("id"));
+				offer.setName(rs.getString("name"));
+				offer.setEmail(rs.getString("email"));
+				offer.setText(rs.getString("text"));
+				
+				return offer;
+			}
+
+		});
+		
+		return offers.size() != 0 ? offers.get(0) : null;
 	}
 
 }
